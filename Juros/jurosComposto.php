4d8cccc689325113c128351580d8fea78b109998
Marvin <escorpiano18@bol.com.br>
2024-10-21 10:55:02 -0300

    atualização

--- Juros/jurosComposto.php
+++ Juros/jurosComposto.php
@@ -6,4 +6,8 @@
     echo "Capital: $capital <br>";
     echo "Taxa: $taxa <br>";
     echo "Tempo: $tempo <br>";
+
+    $montante = $capital * pow(1+$taxa, $tempo);
+    // Pode usar tambem da seguinte maneira: $montante = $capital * (1+$taxa) ** $tempo;
+    echo "Montante: $montante <br>";
 ?>
